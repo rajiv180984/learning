063c11321732ad5523c28157b9c22c122726c210
rajiv180984 <rathor.rajeev@gmail.com>
2021-01-25 04:05:42 -0500

    Update PositiveAndNigativeSegregate.java

--- src/com/first/array/PositiveAndNigativeSegregate.java
+++ src/com/first/array/PositiveAndNigativeSegregate.java
@@ -2,6 +2,27 @@ package src.com.first.array;
 
 public class PositiveAndNigativeSegregate {
 
-	
+	public static void main(String[] args) {
+			int []a = {-10, -4,20,-1,8,9,11,1};
+			int i=0; int j=a.length-1;
+			while(i<j) {
+				if(a[i]<0) {
+					i++;
+				}else {
+					if(a[j]>0) {
+						j--;
+					}else {
+						int t= a[i];
+						a[i] = a[j];
+						a[j] =t;
+						i++;
+						j--;
+					}
+				}
+			}
+			for(int k: a) {
+				System.out.println(k);
+			}
+	}
 	
 }
